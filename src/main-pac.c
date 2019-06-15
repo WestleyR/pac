d268f835c0a13a9914851137151cff47c1538e9d
WestleyK <westley@sylabs.io>
2019-06-15 11:25:43 -0700

    Added close + updated code

--- src/main-pac.c
+++ src/main-pac.c
@@ -1,7 +1,7 @@
 // created by: WestleyR
 // email: westleyr@nym.hush.com
 // https://github.com/WestleyR/pac
-// date: Feb 21, 2019
+// date: Jun 15, 2019
 // version-1.0.0
 //
 // The Clear BSD License
@@ -21,7 +21,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define SCRIPT_VERSION "v1.0.0-beta-5, Feb 21, 2019"
+#define SCRIPT_VERSION "v1.0.0-beta-6, Jun 15, 2019"
 
 void helpMenu(char* SCRIPT_NAME) {
     printf("USAGE:\n");
@@ -49,13 +49,14 @@ void pac(int input) {
     struct stat filestats;
 
     if (fstat(output, &filestats) == -1) {
+        perror("fstat");
         fprintf(stderr, "%d", errno);
     }
 
     // st_blksize is size_t
     buffer = malloc(filestats.st_blksize);
     if (buffer == NULL) {
-        fprintf(stderr, "SEGMENTATION FAULT.\n");
+        fprintf(stderr, "SEGMENTATION FAULT\n");
         exit(100);
     }
 
@@ -69,15 +70,16 @@ void pac(int input) {
 
 int main(int argc, char *argv[]) {
     char *SCRIPT_NAME = *argv;
-
     int descriptor;
 
     if (argc >= 1) {
         for (int i=1; i < argc; i++) {
             if (strcmp(argv[i], "--help") == 0) {
                 helpMenu(SCRIPT_NAME);
+                break;
             } else if (strcmp(argv[i], "--version") == 0) {
                 versionPrint();
+                break;
             }
         }
     }
@@ -89,31 +91,43 @@ int main(int argc, char *argv[]) {
         pac(descriptor);
     } else {
         argv++;
-        while (*argv) {
-            if (strcmp(*argv, "-") == 0) {
+        while (*argv != NULL) {
+//            if (strcmp(*argv, "-") == 0) {
+            if ((strlen(*argv) == 1) && (*argv[0] == '-')) {
                 descriptor = fileno(stdin);
             } else {
-                if (access(*argv, F_OK ) == -1) {
-                    fprintf(stderr, "%s: %s: No such file or directory.\n", SCRIPT_NAME, *argv);
-                    return(2);
+
+                if (access(*argv, F_OK ) != 0) {
+                    fprintf(stderr, "%s: %s: No such file or directory\n", SCRIPT_NAME, *argv);
+                    argv++;
+                    continue;
                 }
+
                 if (access(*argv, R_OK) != 0) {
-                    fprintf(stderr, "%s: %s: Permision denied.\n", SCRIPT_NAME, *argv);
-                    return(2);
+                    fprintf(stderr, "%s: %s: Permision denied\n", SCRIPT_NAME, *argv);
+                    argv++;
+                    continue;
                 }
+
                 struct stat statbuf;
                 stat(*argv, &statbuf);
                 if (S_ISDIR(statbuf.st_mode) != '\0') {
-                    fprintf(stderr, "%s: %s: Is a directory.\n", SCRIPT_NAME, *argv);
-                    return(2);
+                    fprintf(stderr, "%s: %s: Is a directory\n", SCRIPT_NAME, *argv);
+                    argv++;
+                    continue;
+//                    return(2);
                 }
                 descriptor = open(*argv, O_RDONLY);
             }
+//            printf("INFOP: %i\n", descriptor);
             if (descriptor < 0) {
-                fprintf(stderr, "ERROR: An error has occured: unable to read file.\n");
-                return(2);
+                fprintf(stderr, "%s: unable to open file: %s: %s\n", SCRIPT_NAME, *argv, strerror(errno));
+                argv++;
+                continue;
+//                return(2);
             }
             pac(descriptor);
+            close(descriptor);
             argv++;
         }
     }
